d0484773962068e8592e95fe006671c59b889fb9
Brayan Steven Arboleda Gonzalez <brayantseven17@gmail.com>
2026-04-24 14:40:29 -0500

    Creación del carrusel y actualización de rutas
    Se realizo la creación del carrusel del home y se actualizaron las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,34 @@
-import {  BrowserRouter, Routes, Route } from "react-router-dom"
-import Navbar from "./components/Navbar"
-import Footer from "./components/Footer"
-import Inicio from "./pages/Inicio"
-import Menu from "./pages/Menu"
-import Nosotros from "./pages/Nosotros"
-import Reservas from "./pages/Reservas"
-import Domicilios from "./pages/Domicilios"
-import Comentarios from "./pages/Comentarios"
-import './styles/index.css'
-import './styles/Variables.css'
-import './styles/navbar.css'
-import './styles/footer.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Inicio from "./pages/Inicio/Inicio";
+import Menu from "./pages/Menu/Menu";
+import Nosotros from "./pages/Nosotros/Nosotros";
+import Reservas from "./pages/Reservas/Reservas";
+import Domicilios from "./pages/Domicilios/Domicilios";
+import Comentarios from "./pages/Comentarios/Comentarios";
+
+import "foundation-sites/dist/css/foundation.min.css";
+import "./styles/index.css";
+import "./styles/Variables.css";
 
 function App() {
-
   return (
     <BrowserRouter>
       <Navbar />
       <div className="main-content">
         <Routes>
-            <Route path="/" element={<Inicio />} />
-            <Route path="/menu" element={<Menu />} />
-            <Route path="/nosotros" element={<Nosotros />} />
-            <Route path="/reservas" element={<Reservas />} />
-            <Route path="/comentarios" element={<Comentarios />} />
-            <Route path="/domicilios" element={<Domicilios />} />
+          <Route path="/"            element={<Inicio />}     />
+          <Route path="/menu"        element={<Menu />}        />
+          <Route path="/nosotros"    element={<Nosotros />}    />
+          <Route path="/reservas"    element={<Reservas />}    />
+          <Route path="/comentarios" element={<Comentarios />} />
+          <Route path="/domicilios"  element={<Domicilios />}  />
         </Routes>
       </div>
       <Footer />
     </BrowserRouter>
-
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,59 @@
+import { Link } from "react-router-dom";
+import "./Footer.css";
+import { FaFacebook, FaInstagram, FaTiktok, FaTripadvisor } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="container">
+      <div className="grid-x grid-padding-x">
+
+        {/* Enlaces rápidos */}
+        <div className="cell small-12 medium-4">
+          <h4>Enlaces Rápidos</h4>
+          <ul>
+            <li><Link className="fast-access" to="/">Inicio</Link></li>
+            <li><Link className="fast-access" to="/menu">Menú</Link></li>
+            <li><Link className="fast-access" to="/nosotros">Nosotros</Link></li>
+            <li><Link className="fast-access" to="/reservas">Reservas</Link></li>
+            <li><Link className="fast-access" to="/domicilios">Domicilios</Link></li>
+          </ul>
+        </div>
+
+        {/* Horario y redes */}
+        <div className="cell small-12 medium-4">
+          <h4>Horario de Atención</h4>
+          <span>Lunes - Viernes: 8:00 AM - 4:00 PM</span>
+          <span>Sábado - Domingo: 9:00 AM - 3:00 PM</span>
+          <div className="footer-icons">
+            <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+              <FaFacebook />
+            </a>
+            <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+              <FaInstagram />
+            </a>
+            <a href="https://www.tiktok.com/" target="_blank" rel="noopener noreferrer">
+              <FaTiktok />
+            </a>
+            <a href="https://www.tripadvisor.com/" target="_blank" rel="noopener noreferrer">
+              <FaTripadvisor />
+            </a>
+          </div>
+        </div>
+
+        {/* Contacto */}
+        <div className="cell small-12 medium-4">
+          <h4>Contáctenos</h4>
+          <span>Tel: +57 0000000000</span>
+          <span>Email: info@brunchcafe.com</span>
+          <span>Dirección: Carrera 36B #30-06</span>
+        </div>
+
+      </div>
+      <div className="footer-bottom">
+        <span>© 2026 Brunch Café | Todos los derechos reservados</span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
