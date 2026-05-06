13c3d7b9ab5119939bd3ead84a9529d0f452c033
Jose David Garcia <garciajosedavid2005@gmail.com>
2026-05-06 18:42:17 -0500

    add pagina nosotros hero historia ubicacion fondo animado reservas

--- src/pages/Reservas/Reservas.jsx
+++ src/pages/Reservas/Reservas.jsx
@@ -2,13 +2,16 @@ import "./Reservas.css"
 import HeroReservas from "../../components/HeroReservas/HeroReservas"
 import FormularioReservas from "../../components/FormularioReservas/FormularioReservas"
 import ContactoReservas from "../../components/ContactoReservas/ContactoReservas"
+import FondoAnimado from "../../components/FondoAnimado/FondoAnimado"
 
 const Reservas = () => {
     return (
         <main className="pagina-Reservas">
             <HeroReservas />
-            <FormularioReservas />
-            <ContactoReservas />
+            <FondoAnimado>
+                <FormularioReservas />
+                <ContactoReservas />
+            </FondoAnimado>
         </main>
 
     )
